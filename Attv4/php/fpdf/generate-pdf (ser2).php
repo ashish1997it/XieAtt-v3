8dd5db9b5bc611c199613ba72637c59d2916613a
Ashish Sondagar <sondagarashish@gmail.com>
2017-07-26 05:46:54 +0530

    refer this v4
    modify this

---
+++ Attv4/php/fpdf/generate-pdf (ser2).php
@@ -0,0 +1,47 @@
+<?php
+require('mysql_table.php');
+class PDF extends PDF_MySQL_Table
+{
+function Header()
+{
+	$this->Image('logo.jpg',56,7,18);
+	$this->SetFont('Arial','',24);
+	$this->Cell(0,7,'Xie Attendence',0,1,'C');
+	$this->Ln(10);
+	//Ensure table header is output
+	parent::Header();
+}
+
+function Footer()
+{
+    $this->SetY(-15);
+    $this->SetFont('Arial','I',10);
+    $this->Cell(0,10,'Page '.$this->PageNo(),0,0,'R');
+}
+}
+//Connect to database
+mysqli_connect('fdb6.biz.nf','2221790_att','XieAshish007','2221790_att');
+$pdf=new PDF();
+$pdf->AddPage();
+//First table: put all columns automatically
+//public $idNoP;
+//$pdf->MultiCell(0,10,'$idNoP2',0,'J');
+$idNoP2 = $_POST['idNo'];
+//$idNoP2 = '1672';
+//echo "<br> $idNoP2 <br>";
+
+$query ="SELECT idNo, date, day, time, attP from atttab WHERE idNo=$_POST[idNo] order by date";
+
+$pdf->Table($query);
+//Second table: specify 3 columns
+$pdf->AddCol('idNo',40,'','C');
+$pdf->AddCol('date',40,'atttab','C');
+$pdf->AddCol('day',40,'','C');
+$pdf->AddCol('time',40,'','C');
+$pdf->AddCol('attP',40,'','C');
+//$pdf->AddCol('info',40,'','C');
+//$pdf->Output("C:\Users\John\Desktop/somename.pdf",'F'); 
+//header('Content-type: projekter/pdf');
+$pdf->Output('XieAttendence'.".pdf", 'D'); 
+//header('Location: '.projekter.".pdf");
+?>
